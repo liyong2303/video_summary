327b2a15d6d550e2b968a422e415f2a041247780
luyong <340571998@qq.com>
2026-05-04 09:15:31 +0800

    feat(v1.3): 创建模板和快捷操作 Mapper 接口

---
+++ video-summary-service/src/main/java/com/videosummary/mapper/TemplateCategoryMapper.java
@@ -0,0 +1,9 @@
+package com.videosummary.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.videosummary.entity.TemplateCategory;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TemplateCategoryMapper extends BaseMapper<TemplateCategory> {
+}
---
+++ video-summary-service/src/main/java/com/videosummary/mapper/TemplateMapper.java
@@ -0,0 +1,9 @@
+package com.videosummary.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.videosummary.entity.Template;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TemplateMapper extends BaseMapper<Template> {
+}
